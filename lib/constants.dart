13541edf6dae17823bfd7f83dfc5359268c100ba
Hamza <j.hamza@hotmail.fr>
2021-06-27 14:34:40 +0100

    update service service

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,8 +1,9 @@
 import 'package:issaf/language/appLocalizations.dart';
 import 'package:flutter/material.dart';
 
-//const URL_BACKEND = "http://10.0.2.2:8000/api/";
-const URL_BACKEND = "https://issaf.herokuapp.com/api/";
+const URL_BACKEND = "http://10.0.2.2:8000/api/";
+//const URL_BACKEND = "http://127.0.0.1:8000/api/";
+//const URL_BACKEND = "https://issaf.herokuapp.com/api/";
 //TEXT INPUT DECORATION
 inputTextDecorationRounded(
     Icon prefixIcon, String hintText, GestureDetector suffixIcon) {
--- lib/services/serviceService.dart
+++ lib/services/serviceService.dart
@@ -22,10 +22,7 @@ class ServiceService {
   //get service related to operator
   Future<http.Response> getServiceByRespo(String token) async {
     var url = URL_BACKEND + "getServiceByRespo/?api_token=" + token;
-    return await http.get(url, headers: {
-      "Accept": "application/json",
-      "Access-Control_Allow_Origin": "*"
-    });
+    return await http.get(url);
   }
 
 //add or update service data
